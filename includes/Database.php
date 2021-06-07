ebaec07b11e316bb6531c6cded83fe92cb154356
Rajkumar Sony <rk.sony4848@gmail.com>
2021-06-08 02:30:14 +0530

    done

---
+++ includes/Database.php
@@ -0,0 +1,28 @@
+<?php
+class Database
+{
+
+    // private $dbServer = 'localhost';
+    // private $dbUser = 'root';
+    // private $dbPassword = '';
+    // private $dbName = 'playersdb';
+    // protected $conn;
+
+    private $dbServer = 'remotemysql.com';
+    private $dbUser = 'TYwiSXWJQK';
+    private $dbPassword = 'DeJfmIgZLS';
+    private $dbName = 'TYwiSXWJQK';
+    protected $conn;
+
+    public function __construct()
+    {
+        try {
+            $dsn = "mysql:host={$this->dbServer}; dbname={$this->dbName}; charset=utf8";
+            $options = array(PDO::ATTR_PERSISTENT);
+            $this->conn = new PDO($dsn, $this->dbUser, $this->dbPassword, $options);
+        } catch (PDOException $e) {
+            echo "Connection Error: " . $e->getMessage();
+        }
+
+    }
+}
